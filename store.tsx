a416fd0457971db7b7ad9028614c0b4af97298e0
Mitra Bauer <mitra.bauer@appshack.se>
2020-05-25 15:33:00 +0200

    Remove unused dependencies

--- store.tsx
+++ store.tsx
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import rootReducer from "./reducers";
@@ -10,7 +10,7 @@ export const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-let store = createStore(persistedReducer, applyMiddleware(thunk));
+let store = createStore(persistedReducer);
 let persistor = persistStore(store);
 
 export { store, persistor };
